9200d01770d2c5746d89be1bb2fb92ad4f9e6f47
santhoshsis <santhosh.y@sourceitsolutions.com>
2025-11-24 17:04:02 -0500

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
+import Body from "./Body"
+import Login from "./Login"
+import Signup from "./Signup"
+import Profile from "./Profile"
 
 function App() {
 
   return (
     <>
-      <h1>Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
